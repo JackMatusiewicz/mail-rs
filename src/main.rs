44a1f6f9bd18ffd70ea46ee06358d8069342f01d
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2024-02-25 14:22:06 +0000

    Adding the first endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/echo/{name}")]
+async fn index(req: HttpRequest) -> impl Responder {
+    let response = format!("Hello, {}", req.match_info().get("name").unwrap_or("anon"));
+    HttpResponse::Ok().body(response)
+}
+
+async fn manual_greet() -> impl Responder {
+    HttpResponse::Ok().body("manual response")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .route("/manual", web::get().to(manual_greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
